ab4fcc7975d0f6387b2d681f22bf70ca751d7aae
Rafael Saca <raffaellsaca@gmail.com>
2024-10-17 12:07:29 -0300

    feat: Tela ACCOUNT

---
+++ src/screens/account/account.js
@@ -0,0 +1,44 @@
+import { Image, Text, TextInput, TouchableOpacity, View } from "react-native";
+import icon from "../../constants/icon.js";
+import { styles } from "./account.style";
+import Button from "../../components/button/button";
+
+export function Account() {
+  return (
+    <>
+      <View style={styles.container}>
+        <View style={styles.containerLogo}>
+          <Image source={icon.logo} style={styles.logo} />
+        </View>
+
+        <View>
+
+          <View style={styles.containerInput}>
+            <TextInput placeholder="Nome" style={styles.input} />
+          </View>
+
+          <View style={styles.containerInput}>
+            <TextInput placeholder="E-mail" style={styles.input} />
+          </View>
+
+          <View style={styles.containerInput}>
+            <TextInput
+              placeholder="Senha"
+              style={styles.input}
+              secureTextEntry={true}
+            />
+          </View>
+
+          <Button text="Acessar" />
+        </View>
+
+        <View style={styles.footer}>
+          <Text>Já tenho conta.</Text>
+          <TouchableOpacity>
+            <Text style={styles.footerLink}>Fazer login.</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </>
+  );
+}
