fb18e52f84f77b4a7f4b3d5b864f3f6a78d582ce
Rafael Saca <raffaellsaca@gmail.com>
2024-10-31 22:54:34 -0300

    feat: Container de navegação com Routes

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-import Button from "./src/components/button/button.jsx";
-import {Login} from "./src/screens/login/login.jsx"
-import { Account } from "./src/screens/account/account.js";
-import { Home } from "./src/screens/home/home.jsx";
-import { Calendar } from "./src/screens/calendar/calendar.jsx";
-import {Profile} from "./src/screens/profile/profile.jsx";
-import {Main} from "./src/screens/main/main.jsx";
-import { Services } from "./src/screens/services/services.jsx";
-import Schedule from "./src/screens/schedule/schedule.jsx";
+import Routes from "./src/routes/routes.js";
+import { NavigationContainer } from "@react-navigation/native";
+
 export default function App() {
+  return (
+       <NavigationContainer>
+        <Routes />
+      </NavigationContainer>
 
-  return <>  
-
-    <Schedule/>
-
-  </>;  
+  );
 }
