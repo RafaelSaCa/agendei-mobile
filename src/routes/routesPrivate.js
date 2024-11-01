7a78c6d27ae12acf2b6f5383f1bbc9be7a76424c
Rafael Saca <raffaellsaca@gmail.com>
2024-10-31 22:57:48 -0300

    feat: Rotas privadas e públicas

---
+++ src/routes/routesPrivate.js
@@ -0,0 +1,40 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {Main} from "../screens/main/main.jsx"
+import {Services} from "../screens/services/services.jsx";
+import Schedule from "../screens/schedule/schedule.jsx";
+import { COLORS } from '../constants/theme.js';
+
+const Stack = createNativeStackNavigator();
+
+function RoutesPrivate(){
+  return <Stack.Navigator>
+        <Stack.Screen name="main" component={Main}
+         options={{
+          headerShown: false,
+          headerTitle: "Médicos",
+        }}/>
+
+        <Stack.Screen name="services" component={Services}
+         options={{
+            headerTitle: "Serviços",
+            headerTitleAlign: "center",
+            headerShadowVisible: false,
+            headerTintColor: COLORS.white,
+            headerStyle:{
+              backgroundColor: COLORS.blue
+
+            }
+        }}/>
+
+        <Stack.Screen name="schedule" component={Schedule}
+         options={{
+          headerTitle: "Reserva",
+            headerTitleAlign: "center",
+            headerShadowVisible: false,
+            headerTintColor: COLORS.blue,
+          }}/>
+ 
+      </Stack.Navigator>
+}
+
+export default RoutesPrivate;
