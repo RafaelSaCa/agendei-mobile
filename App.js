ed3d4343bedeed825e5936a4e9c0c01d037ad345
Rafael Saca <raffaellsaca@gmail.com>
2024-11-02 09:41:55 -0300

    feat: Encapsulando a aplicação com o contexto provider

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import Routes from "./src/routes/routes.js";
 import { NavigationContainer } from "@react-navigation/native";
+import { AuthProvider } from "./src/context/auth.js";
 
 export default function App() {
   return (
-       <NavigationContainer>
-        <Routes />
-      </NavigationContainer>
+    <NavigationContainer>
 
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
+
+      
+    </NavigationContainer>
   );
 }
