e1cd7885b9eeb607061ef21859f95833bd26a47e
Rafael Saca <raffaellsaca@gmail.com>
2024-11-03 23:04:04 -0300

    feat: Setando a data e hora do agendamento

--- src/screens/schedule/schedule.jsx
+++ src/screens/schedule/schedule.jsx
@@ -1,10 +1,12 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import { styles } from "./schedule.style.js";
 import { Calendar, LocaleConfig } from "react-native-calendars";
 import { ptBR } from "../../constants/calendar.js";
 import { useState } from "react";
 import { Picker } from "@react-native-picker/picker";
 import Button from "../../components/button/button.jsx";
+import api from "../../constants/api.js";
+
 
 LocaleConfig.locales["pt-br"] = ptBR;
 LocaleConfig.defaultLocale = "pt-br";
@@ -17,44 +19,65 @@ function Schedule(props) {
   const [selectedDate, setSelectDate] = useState("");
   const [selectedHour, setSelectHour] = useState("");
 
-  function ClickBooking(){
-    console.log(id_doctor, id_service, selectedDate, selectedHour)
-  }
+        
+  async function ClickBooking(){
+    try {
+      const response = await api.post("/appointments",{
+        id_doctor, id_service, booking_Date: selectedDate , booking_Hour: selectedHour
+      });
+
+      if (response.data?.id_appointment){       
+          props.navigation.popToTop();
+    
+      }
+
+    } catch (error) {
+      if (error.response?.data.error)
+         Alert.alert(error.response.data.error)
+        
+      else
+         Alert.alert("Ocorreu um erro. Tente novamente mais tarde!");
+
+    }
+}
+
 
   return (
     <View style={styles.container}>
-      <View>
-        <Calendar style={styles.theme}
-          onPress={(day) => {
-                setSelectDate(day.dateString);
-          }}
-          markedDates={{ [selectedDate]: { selected: true } }}
-          minDate={new Date().toDateString()}
-        />
-
         <View>
-          <Text style={styles.textHour}>Horário</Text>
+            <Calendar theme={styles.theme}
+                onDayPress={(day) => {
+                  setSelectDate(day.dateString)
+                }}
+                markedDates={{
+                    [setSelectDate]: { selected: true }
+                }}
+
+                minDate={new Date().toDateString()}
+            />
+
+            <View>
+                <Text style={styles.textHour}>Horário</Text>
+            </View>
+
+
+            <View>
+                <Picker selectedValue={setSelectHour}
+                    onValueChange={(itemValue, itemIndex) => {
+                      setSelectHour(itemValue)
+                    }}>
+                    <Picker.Item label="09:00" value="09:00" />
+                    <Picker.Item label="09:30" value="09:30" />
+                    <Picker.Item label="10:00" value="10:00" />
+                </Picker>
+            </View>
+
         </View>
 
         <View>
-          <Picker
-            selectedValue={selectedHour}
-            onValueChange={(itemValue, itemIndex) => {
-              setSelectHour(itemValue);
-            }}
-          >
-            <Picker.Item label="09:00" value="09:00" />
-            <Picker.Item label="09:30" value="09:30" />
-            <Picker.Item label="10:00" value="10:00" />
-            <Picker.Item label="10:30" value="10:30" />
-          </Picker>
+            <Button text="Confirmar Reserva" onPress={ClickBooking} />
         </View>
 
-      </View>
-
-      <View>
-        <Button text="Confirmar Reserva" onPress={ClickBooking} />
-      </View>
     </View>
   );
 }
