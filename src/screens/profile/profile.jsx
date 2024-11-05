daa636855d38e2c4e24584efdf6e2305e802b4e2
Rafael Saca <raffaellsaca@gmail.com>
2024-11-04 23:05:07 -0300

    feat: Carrega dados do perfil do usuário

--- src/screens/profile/profile.jsx
+++ src/screens/profile/profile.jsx
@@ -1,20 +1,66 @@
-import { Text, View } from "react-native";
+import { Alert, Text, View } from "react-native";
 import { styles } from "./profile.style";
-
+import api from "../../constants/api";
+import { useContext, useEffect, useState } from "react";
+import Button from "../../components/button/button";
+import {AuthContext} from "../../context/auth";
 
 export function Profile(){
+
+  const {setUser} = useContext(AuthContext);
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  async function LoadProfile(){
+    try {
+      const response = await api.get("/users/profile");
+     
+      if (response.data?.name)
+          setName(response.data.name);
+          setEmail(response.data.email);
+      
+
+    } catch (error) {
+      if (error.response?.data.error){
+         Alert.alert(error.response.data.error)
+      }
+      else
+         Alert.alert("Ocorreu um erro. Tente novamente mais tarde!");
+
+    }
+  }
+
+  function Logout(){
+    api.defaults.headers.common['Authorization'] = "";//remove o token para deslogar o usuario.
+    setUser({});
+  }
+
+  useEffect(()=>{
+    LoadProfile();
+  });
+
+
+
   return (
     <>
       <View style={styles.container}>
         <View style={styles.item}>
           <Text style={styles.title}>Nome</Text>
-          <Text style={styles.text}>Rafael SaCa</Text>
+          <Text style={styles.text}>{name}</Text>
         </View>
         
         <View style={styles.item}>
           <Text style={styles.title}>E-mail</Text>
-          <Text style={styles.text}>rafael@gmail</Text>
+          <Text style={styles.text}>{email}</Text>
         </View>
+
+        
+        <View style={styles.item}>
+          <Button text="Desconectar" theme="danger" onPress={Logout}/>
+
+        </View>
+
       </View>
     </>
   );
