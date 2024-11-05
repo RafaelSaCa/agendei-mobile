2d97c70098c929be37974154601ed9e7d0502f7a
Rafael Saca <raffaellsaca@gmail.com>
2024-11-04 23:02:08 -0300

    feat: Lista e Remove agengamento do usuário

--- src/screens/calendar/calendar.jsx
+++ src/screens/calendar/calendar.jsx
@@ -1,22 +1,73 @@
-import { FlatList, Text, View } from "react-native";
+import { Alert, FlatList, View } from "react-native";
 import { styles } from "./calendar.style.js"
-import { appointments } from "../../constants/data.js";
+import api from "../../constants/api.js";
 import Appointment from "../../components/appointment/appointment.jsx";
-import icon from "../../constants/icon.js";
+import { useEffect, useState } from "react";
+
 
 export function Calendar() {
+
+  const [appointments, setAppointments] = useState([]);
+  
+  async function LoadAppointments(){
+    try {
+      const response = await api.get("/appointments");
+     
+      if (response.data)
+         setAppointments(response.data);
+      
+
+    } catch (error) {
+      if (error.response?.data.error){
+         Alert.alert(error.response.data.error)
+      }
+      else
+         Alert.alert("Ocorreu um erro. Tente novamente mais tarde!");
+
+    }
+  }
+
+  
+  async function DeleteAppointment(id_appointment){
+    try {
+      const response = await api.delete("/appointments/"+id_appointment);
+     
+      if (response.data?.id_appointment)
+          LoadAppointments();
+      
+
+    } catch (error) {
+      if (error.response?.data.error){
+         Alert.alert(error.response.data.error)
+      }
+      else
+         Alert.alert("Ocorreu um erro. Tente novamente mais tarde!");
+
+    }
+  }
+  
+  useEffect(() =>{
+    LoadAppointments();
+  }, []);
+
+
+
   return (
         <>
         <View style={styles.container}>
           
            <FlatList data={appointments}
-                     keyExtractor={(appoint) => appoint.id_doctor}
+                     keyExtractor={(appoint) => appoint.id_appointment}
                      showsVerticalScrollIndicator={false}
-                     renderItem={({item}) => {
+                     renderItem={({ item }) => {
                         return <Appointment 
-                                service={item.service}
+                                id_appointment={item.id_appointment}
                                 doctor={item.doctor}
-                                specialty={item.specialty}/>
+                                service={item.service}                                
+                                specialty={item.specialty}
+                                bookingDate={item.booking_date}
+                                bookingHour={item.booking_hour}
+                                onPress={DeleteAppointment} />
                      }} />
         </View>
         
